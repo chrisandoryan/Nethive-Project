5fc196a760be0d3e8a29a2a6900eb000cf9f6a80
Siahaan <chrisando.siahaan@gmail.com>
2019-10-08 05:50:15 -0700

    added multiple filters in logstash

--- parsers/bash_parser.py
+++ parsers/bash_parser.py
@@ -1,6 +1,10 @@
 import settings
+from utils import tail
 
 def run():
+    print("[*] Starting BashParser Engine...")
+    logfile = open("", 'r')
+    loglines = tail(logfile)
     return
 
 if __name__ == "__main__":
