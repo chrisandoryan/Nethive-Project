fe434e50ce9b94beb7321c5f418dd31c98c3d3c2
Chrisando Ryan <chrisando.siahaan@gmail.com>
2019-12-09 08:05:26 +0700

    updated env configuration, deleted unused files

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt update && apt install mysql-server libmysqlclient-dev libxml2-dev curl apt-transport-https ca-certificates curl software-properties-common snapd pkg-config -y
+sudo apt update && apt install mysql-server libmysqlclient-dev libxml2-dev curl apt-transport-https ca-certificates curl software-properties-common snapd pkg-config python3-pip -y
 
 go get github.com/jbowtie/gokogiri
