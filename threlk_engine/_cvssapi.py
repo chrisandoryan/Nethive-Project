8bae5dd7e8efda0e44f5385171350b67d9cd3a2a
root <git@rk-sragen.site>
2020-09-11 22:58:30 +0700

    Replaced _id with time of attack

--- threlk_engine/_cvssapi.py
+++ threlk_engine/_cvssapi.py
@@ -2,24 +2,18 @@
 
 import datetime
 def convert(result):
-		print("result {}".format(result))
+		id = result["EVENT_DATA"]["arrived_at"]
 		xss_data = result["EVENT_DATA"]["audit_xss"]
 		sqli_data = result["EVENT_DATA"]["inspect_sqli"]
-		print("XSS {}".format(xss_data))
-		print("SQL {}".format(sqli_data))
 		""" check for True result """
 		xss_malicious = []
 		sqli_malicious = []
 		if xss_data != None:
-			print("Scan XSS")
 			if xss_data.get("QueryParamAuditResult") and xss_data["QueryParamAuditResult"]!=None:
 				xss_malicious = [items for items in xss_data["QueryParamAuditResult"] if items["LikelyMalicious"]==True]
 		if sqli_data != None:
-			print("Scan SQL")
 			sqli_malicious = [items for items in sqli_data["inspection_result"] if items["classification"] == "malicious"]
-		print("TEST")
-		print(xss_malicious)
-		print(sqli_malicious)
+
 		# sqli_malicious = [items for items in inspect_sqli]
 		def checkIfNone(ip):
 			if ip == None:
@@ -27,8 +21,8 @@ def convert(result):
 			return ip;
 		converted = []
 		if len(xss_malicious) > 0:
-			converted = [{"_id":1335,"url":items["SinkholePath"],"ip":checkIfNone(items["ClientIP"]), "vul": "XSS","timestamp":str(datetime.datetime.now())} for items in xss_malicious]
+			converted = [{"_id":id,"url":items["SinkholePath"],"ip":checkIfNone(items["ClientIP"]), "vul": "XSS","timestamp":str(datetime.datetime.now())} for items in xss_malicious]
 		if len(sqli_malicious) > 0:
-			converted = [{"_id":1335,"url":sqli_data["url"],"ip":checkIfNone(sqli_data["client_ip"]), "vul": "SQLi","timestamp":str(datetime.datetime.now())}]
+			converted = [{"_id":id,"url":sqli_data["url"],"ip":checkIfNone(sqli_data["client_ip"]), "vul": "SQLi","timestamp":str(datetime.datetime.now())}]
 
 		return converted;
